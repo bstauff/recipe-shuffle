f11d06ec30305247cfaaa2682a8f4e275018923d
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2023-07-06 07:39:51 -0500

    adding shuffle component

--- src/app/meal-plan/meal-plan.module.ts
+++ src/app/meal-plan/meal-plan.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { MealPlanRoutingModule } from './meal-plan-routing.module';
 import { MealPlanComponent } from './meal-plan.component';
+import { ShuffleComponent } from './shuffle/shuffle.component';
 
 
 @NgModule({
   declarations: [
-    MealPlanComponent
+    MealPlanComponent,
+    ShuffleComponent
   ],
   imports: [
     CommonModule,
