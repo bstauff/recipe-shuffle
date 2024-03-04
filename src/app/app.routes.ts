67eca9e25fe5ed03c8c97b69b6fd0b7edb05da25
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2024-03-04 09:01:24 -0600

    converting routing modules to app.routes.ts

---
+++ src/app/app.routes.ts
@@ -0,0 +1,48 @@
+import { Routes } from '@angular/router';
+import { LoginGuard } from './account/login.guard';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './account/login/login.component';
+import { NewPasswordComponent } from './account/password-reset/new-password/new-password.component';
+import { PasswordResetComponent } from './account/password-reset/password-reset.component';
+import { RegisterComponent } from './account/register/register.component';
+import { MealPlanComponent } from './meal-plan/meal-plan.component';
+
+export const routes: Routes = [
+  {
+    path: 'recipes',
+    component: RecipeListComponent,
+    canActivate: [LoginGuard],
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      {
+        path: 'password-reset',
+        component: PasswordResetComponent,
+      },
+      {
+        path: 'password-reset/new-password',
+        component: NewPasswordComponent,
+      },
+    ],
+  },
+  {
+    path: 'meal-plan',
+    component: MealPlanComponent,
+    canActivate: [LoginGuard],
+  },
+  {
+    path: '**',
+    redirectTo: 'recipes',
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,7 @@
-import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AccountModule } from './app/account/account.module';
-import { RecipeModule } from './app/recipe/recipe.module';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      BrowserModule,
-      AppRoutingModule,
-      RecipeModule,
-      AccountModule,
-      ReactiveFormsModule,
-      MatSlideToggleModule,
-      MatSidenavModule,
-      MatToolbarModule,
-      MatListModule,
-      MatIconModule,
-      MatButtonModule,
-      MatTabsModule,
-      MatDividerModule,
-      MatSnackBarModule
-    ),
-    provideAnimations(),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
