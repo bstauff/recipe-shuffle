eed51dfb146808afe2ac6ce7dc8b1a92a5593776
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2024-04-23 12:46:09 -0500

    removing id128

--- src/app/recipe/models/recipe.ts
+++ src/app/recipe/models/recipe.ts
@@ -1,18 +1,8 @@
-import { Ulid, Uuid } from 'id128';
 import { RecipeIngredient } from './recipeingredient';
 
 export class Recipe {
-  key: string;
+  id: number;
   name: string;
   url: string | null | undefined;
   recipeIngredients: RecipeIngredient[] = [];
-
-  constructor(name: string, url: string | null | undefined) {
-    const ulidKey = Ulid.generate().toRaw();
-    const uuidKey = Uuid.fromRaw(ulidKey);
-
-    this.key = uuidKey.toCanonical();
-    this.name = name;
-    this.url = url;
-  }
 }
