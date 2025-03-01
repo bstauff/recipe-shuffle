dca90a79af67de16f2ef2a87930bb704731f9e41
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2025-02-28 22:52:29 -0600

    bringing in firebase sdk and angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,17 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
---
+++ src/environments/environment.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'your-project-id',
+    appId: 'your-app-id',
+    storageBucket: 'your-storage-bucket',
+    apiKey: 'your-api-key',
+    authDomain: 'your-auth-domain',
+    messagingSenderId: 'your-messaging-sender-id',
+    measurementId: 'your-measurement-id',
+  }
+};
