475a877c5556bf4745a4753004fd95187de7dc64
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2025-04-09 21:18:58 -0500

    adding login redirect if not logged in

--- src/app/shared/auth/auth.guard.ts
+++ src/app/shared/auth/auth.guard.ts
@@ -1,8 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from './auth.service';
+import { exhaustMap, from, of, tap } from 'rxjs';
+
 
 export const authGuard: CanActivateFn = () => {
-  // const router = inject(Router);
-  // const authService = inject(AuthService);
+  const router = inject(Router);
+  const authService = inject(AuthService);
 
-  return false;
+  return authService.isLoggedIn$.pipe(
+    exhaustMap((isLoggedIn: boolean) => {
+      if (!isLoggedIn) {
+        return router.navigate(['/account/login']);
+      } else {
+        return of(true)
+      }
+    })
+  );
 };
