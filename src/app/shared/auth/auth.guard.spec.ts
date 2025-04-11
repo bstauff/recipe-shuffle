cb4a9b7990b0f007f224dc5d562a267b861532ba
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2025-04-10 21:38:54 -0500

    writing tests

--- src/app/shared/auth/auth.guard.spec.ts
+++ src/app/shared/auth/auth.guard.spec.ts
@@ -1,6 +1,27 @@
+import { TestBed } from "@angular/core/testing";
 import { authGuard } from "./auth.guard";
+import { AuthService } from "./auth.service";
+import { of } from "rxjs";
+import { CanActivateFn } from "@angular/router";
 
-describe('AuthGuard', () => {
-  // need to setup testbed with router
-  // and auth service mock
+describe("AuthGuard", () => {
+	let authServiceMock: jasmine.SpyObj<AuthService> = jasmine.createSpyObj(
+		"AuthService",
+		["isLoggedIn$"],
+		{
+			isLoggedIn$: of(false),
+		},
+	);
+
+	let guard: CanActivateFn;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({ providers: [AuthService] });
+
+		guard = TestBed.inject(authGuard);
+	});
+
+	it("should do stuff", () => {
+		expect(true).toBe(true);
+	});
 });
