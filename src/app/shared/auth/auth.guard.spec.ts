2175f3cab9a3e17f9b2f1627b2b0ea5b0ab23b61
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2025-04-13 12:53:39 -0500

    switching to vitest

--- src/app/shared/auth/auth.guard.spec.ts
+++ src/app/shared/auth/auth.guard.spec.ts
@@ -3,25 +3,37 @@ import { authGuard } from "./auth.guard";
 import { AuthService } from "./auth.service";
 import { of } from "rxjs";
 import { CanActivateFn } from "@angular/router";
+import { SupabaseService } from "../supabase.service";
+import { vi } from "vitest";
 
 describe("AuthGuard", () => {
-	let authServiceMock: jasmine.SpyObj<AuthService> = jasmine.createSpyObj(
-		"AuthService",
-		["isLoggedIn$"],
-		{
-			isLoggedIn$: of(false),
-		},
-	);
+	vi.mock(import("./auth.service"), () => {
+		const AuthService = vi.fn();
+		AuthService.prototype.$isLoggedIn = vi.fn();
+		return { AuthService };
+	});
+	const authServiceMock = new AuthService();
 
-	let guard: CanActivateFn;
+	vi.mock(import("../supabase.service"), () => {
+		const SupabaseService = vi.fn();
+		SupabaseService.prototype.$user = vi.fn();
+		return { SupabaseService };
+	});
+	const supabaseMock = new SupabaseService();
 
 	beforeEach(() => {
-		TestBed.configureTestingModule({ providers: [AuthService] });
-
-		guard = TestBed.inject(authGuard);
+		TestBed.configureTestingModule({
+			providers: [
+				{ provide: AuthService, useValue: authServiceMock },
+				{ provide: SupabaseService, useValue: supabaseMock },
+			],
+		});
 	});
 
 	it("should do stuff", () => {
+		TestBed.runInInjectionContext(() => {
+			// authGuard(activatedRouteSnapshot, routeStateSnapshot);
+		});
 		expect(true).toBe(true);
 	});
 });
---
+++ vite.config.mts
@@ -0,0 +1,31 @@
+/// <reference types="vitest" />
+
+import angular from "@analogjs/vite-plugin-angular";
+
+import { defineConfig } from "vite";
+import * as path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+	return {
+		plugins: [angular()],
+		test: {
+			globals: true,
+			environment: "jsdom",
+			setupFiles: ["src/test-setup.ts"],
+			include: ["**/*.spec.ts"],
+			reporters: ["default"],
+		},
+		define: {
+			"import.meta.vitest": mode !== "production",
+		},
+		resolve: {
+			alias: {
+				"src/environments/environment": path.resolve(
+					__dirname,
+					"src/environments/environment.ts",
+				),
+			},
+		},
+	};
+});
